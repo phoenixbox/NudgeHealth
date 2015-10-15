f026dd86388cc1e652c103870c12ecacad270a5e
Shane Rogers <shane@repllabs.com>
2015-10-15 12:26:58 -0700

    Project init with gitignore

--- NudgeHealth/ViewController.swift
+++ NudgeHealth/ViewController.swift
@@ -7,9 +7,48 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
-
+    
+    let heightQuantity = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight)
+    let weightQuantity = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass)
+    let heartRateQuantity = HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)
+    
+    lazy var healthStore = HKHealthStore()
+    
+    // Not to be written to the health store - share === written
+    lazy var typesToShare: NSSet = {
+        return NSSet(objects: self.heightQuantity!, self.weightQuantity!)
+    }()
+    
+    // Data to read
+    lazy var typesToRead: NSSet = {
+        return NSSet(objects: self.heightQuantity!, self.weightQuantity!, self.heartRateQuantity!)
+    }()
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore.requestAuthorizationToShareTypes(typesToShare as? Set<HKSampleType>,
+                readTypes: typesToRead as? Set<HKObjectType>,
+                completion: {(succeeded: Bool, error: NSError?) -> Void in
+                    if succeeded && error == nil {
+                        print("Auth successful")
+                    } else {
+                        if let theError = error {
+                            print("Error occurred = \(theError)")
+                        }
+                    }
+                }
+            )
+        } else {
+            print("Health data is not available")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
